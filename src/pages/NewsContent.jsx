277aaba8f81a52e5f4206e29d4a6466b5068b5d2
dettah <dettah7@gmail.com>
2024-02-14 11:14:12 +0100

    worked on news content

--- src/pages/NewsContent.jsx
+++ src/pages/NewsContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import BackArrow from '../Components/BackArrow'
-
+import rect from '../assets/rect.png'
 const NewsContent = () => {
     return (
         <div className='px-10'>
@@ -14,12 +14,14 @@ const NewsContent = () => {
 
                 <section className='min-w-[70%]'>
 
-                    <h3 className='text-left text-2xl'>Partners</h3>
-                    <h2 className='text-left text-4xl'>Lorem ipsum dolor sit amet.</h2>
+                    <h3 className='text-left text-2xl mb-4'>Partners</h3>
+                    <h2 className='text-left my-3 text-4xl'>Lorem ipsum dolor sit amet.</h2>
 
-                    <p className='text-left'>
+                    <p className='text-left pr-40 leading-[2] '>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum doloremque quia facilis, nobis sapiente necessitatibus eligendi voluptatum praesentium illo sequi placeat quidem! Voluptatibus, consequuntur esse accusantium provident similique iste quasi nam maiores aperiam iusto commodi quos ducimus ratione ullam sit? Officia optio ex sed doloremque itaque at! <br /> Voluptate nemo aliquid vitae veritatis eligendi obcaecati quis minus provident alias deleniti architecto nobis corrupti, facilis, explicabo rerum quo, modi hic adipisci sequi libero ipsam id quidem? Quisquam tenetur non nam necessitatibus enim sequi suscipit beatae itaque. br
                         Suscipit at similique illo nesciunt provident facilis architecto veniam, distinctio, eaque porro voluptatem obcaecati ducimus neque?
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae laborum fugiat inventore? Ea fugiat ut exercitationem doloribus expedita suscipit aspernatur est eveniet aliquid nam consequatur, odit deserunt dolores id sequi molestias laudantium vel corrupti, nihil maiores modi adipisci reprehenderit magni commodi. Libero consectetur nesciunt molestias repudiandae sequi aperiam obcaecati omnis necessitatibus dignissimos nostrum vitae eaque hic ipsa fugiat asperiores doloribus totam porro deleniti, expedita nihil tenetur perferendis. <br /> Distinctio, exercitationem quibusdam nihil consectetur sed deserunt ut laudantium animi incidunt aperiam, qui repudiandae doloribus, dolor pariatur atque dolores. Fuga nobis fugiat a incidunt! Doloremque voluptatem necessitatibus maiores, praesentium error hic dolores sit harum nesciunt nisi minima, adipisci voluptatibus, excepturi officia expedita et voluptates eligendi porro nostrum cupiditate? <br /> Aperiam voluptatibus quo obcaecati ex necessitatibus voluptates numquam doloremque nostrum? Quaerat praesentium minima ullam ea adipisci dolore corrupti ipsam voluptas similique, aspernatur accusantium assumenda vel delectus atque consequuntur cupiditate totam iusto numquam exercitationem libero in corporis voluptatum perspiciatis! <br /> Tempore ex officiis, magnam tenetur exercitationem totam unde perspiciatis omnis amet praesentium sed animi cumque assumenda, reprehenderit molestias? Exercitationem fuga omnis aut expedita facilis saepe minus quae iusto necessitatibus est non esse repellat error quaerat soluta fugit nulla, sequi quam explicabo? Non consectetur maxime saepe soluta quidem.
+
                     </p>
 
                     <div className='text-left mt-20 font-semibold'>
@@ -27,7 +29,7 @@ const NewsContent = () => {
                         <h4>Tags: HortiNigeria, Greenport, Horticulture, Nigeria, Production</h4>
                     </div>
                     <div className='text-left'>
-                        <h3 className='text-2xl' >RELATED NEWS</h3>
+                        <h3 className='text-2xl mt-10 mb-5 ' >RELATED NEWS</h3>
                         <p>lorem</p>
                         <div>
                             <h3>Balanced diet beyond Nigerian's reach as prices skyrocket</h3>
@@ -54,7 +56,8 @@ const NewsContent = () => {
                         <div className='text-left '>
 
                             <h3 className='font-semibold '>Featured news</h3>
-                            <h2 className='text-2xl'>Lorem ipsum dolor sit amet consectetur.</h2>
+                            <h2 className='text-2xl py-4'>Lorem ipsum dolor sit amet consectetur.</h2>
+                            <img src={rect} alt="" />
                         </div>
                         <div className=''>
 
