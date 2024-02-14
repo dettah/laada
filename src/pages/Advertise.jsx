d10dc71299d615302d46168513c230c32a41ef37
dettah <dettah7@gmail.com>
2024-02-14 11:52:22 +0100

    worked on advert form

--- src/pages/Advertise.jsx
+++ src/pages/Advertise.jsx
@@ -22,17 +22,32 @@ const Advertise = () => {
       <div className='w-[40%] m-auto flex justify-center '>
         <form action="" className='flex justify-start flex-col items-start w-full '>
           <label className='mb-2'>Email Address</label>
-          <input type="email" placeholder='emmanuel@example.com' className='border border-[#6B6C6F] mb-8 px-4 rounded w-full' />
+          <input type="email" placeholder='emmanuel@example.com' className='border outline-none border-[#6B6C6F] py-1 mb-8 px-4 rounded w-full' />
 
           <label className='mb-2'>Select Product</label>
-          <input type="dropdown" className='border py-1  border-[#6B6C6F] rounded w-full mb-8 ' />
+
+          {/*  */}
+
+          <select name="languages" id="lang" className='border py-2 border-[#6B6C6F] rounded w-full mb-8 outline-none px-3 '>
+            <option value="blank"></option>
+            <option value="item1">Advert item one</option>
+            <option value="item2">Advert item two</option>
+            <option value="item3">Advert item three</option>
+            <option value="item4">Advert item four</option>
+          </select>
+
+          {/*  */}
+
+
+
+
 
           <label className='mb-2'>Message</label>
           {/* <textarea placeholder='Message...' className='border border-[#6B6C6F] rounded' /> */}
 
           <textarea
             className=" border border-[#6B6C6F] rounded p-2 h-40 mb-4 w-full"
-            placeholder="message"></textarea>
+            placeholder="message outline-none "></textarea>
 
           <button className='bg-[#E01A4F] text-white rounded py-2 font-bold w-full '>Send</button>
 
