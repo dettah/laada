6f114774079ca412a287dd2f6cc66e002d68e0b7
dettah <dettah7@gmail.com>
2024-02-27 05:10:53 +0100

    responsive community page

--- src/Components/shared/ReuseableCard.jsx
+++ src/Components/shared/ReuseableCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import LearnButton from '../LearnButton'
 const ReuseableCard = ({heading, paragraph, imageUrl}) => {
   return (
-    <div className='flex w-[1169px] h-[546px] gap-11 items-center px-[171px] m-auto shadow-md z-10 hover:scale-110 transition-all duration-300'>
+    <div className='flex xl:w-[1169px] h-[546px] gap-11 items-center xl:px-[171px] m-auto shadow-md z-10 hover:scale-110 hover:bg-[#feeeed]  transition-all duration-300 px-10 flex-col my-28 xl:flex-row xl:my-0 '>
       <div className='flex items-center h-auto'>
-        <img src={`${imageUrl}` }alt="" className='w-[664px] rounded-[8px]' />
+        <img src={`${imageUrl}` }alt="" className='w-[664px] rounded-[8px] ' />
       </div>
-      <div className='w-[433px] text-left leading-[2.2] '>
-        <h3 className='font-semibold text-4xl mb-5'>{heading}</h3>
-        <p className='mb-4 text-xl leading-[1.5]'>
+      <div className='xl:w-[433px] text-left leading-[2.2] '>
+        <h3 className='font-semibold text-[2.25rem] mb-5'>{heading}</h3>
+        <p className='mb-4 text-[1.25rem] leading-[1.5]'>
           {paragraph}
         </p>
         <LearnButton/>
--- src/Components/shared/ReuseableCardOne.jsx
+++ src/Components/shared/ReuseableCardOne.jsx
@@ -3,11 +3,11 @@ import LearnButton from '../LearnButton'
 
 const ReuseableCardOne = ({heading, paragraph, imageUrl}) => {
     return (
-        <div className='flex w-[1169px] h-[546px] gap-11 items-center px-[171px] m-auto shadow-md z-10 hover:scale-110  transition-all duration-300'>
+        <div className='flex xl:w-[1169px] h-[546px] gap-11 items-center xl:px-[171px] m-auto shadow-md z-10 hover:scale-110 hover:bg-[#feeeed]  transition-all duration-300 px-10 flex-col-reverse my-28 xl:flex-row xl:my-0'>
 
-            <div className='w-[433px] text-left leading-[2.2] '>
-                <h3 className='font-semibold mb-5 text-4xl'>{heading}</h3>
-                <p className='mb-4 leading-[1.5] text-xl'>
+            <div className='xl:w-[433px] text-left leading-[2.2] '>
+                <h3 className='font-semibold mb-5 text-[2.25rem]'>{heading}</h3>
+                <p className='mb-4 leading-[1.5] text-[1.25rem]'>
                     {paragraph}
                 </p>
                 <LearnButton />
